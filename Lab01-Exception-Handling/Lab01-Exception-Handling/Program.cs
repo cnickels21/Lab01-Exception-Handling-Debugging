2ec6003ace1ab879b0c8b75186b8acec419461ce
cnickels21 <cnickels_21@yahoo.com>
2020-05-04 18:48:48 -0500

    new array instantiated with user defined length

--- Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
+++ Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
@@ -6,7 +6,19 @@ namespace Lab01_Exception_Handling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+
+            StartSequence();
+
+            Console.ReadLine();
         }
+
+        static void StartSequence()
+        {
+            Console.Write("Please enter a number greater than 0: ");
+            string userDefinedArrayLength = Console.ReadLine();
+            Console.Write(userDefinedArrayLength);
+        }
+
     }
 }
