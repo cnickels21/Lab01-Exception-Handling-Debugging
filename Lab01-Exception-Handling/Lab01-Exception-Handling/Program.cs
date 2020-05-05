2f14f48bb64165a2e52639d6cbc2f3108a89f168
cnickels21 <cnickels_21@yahoo.com>
2020-05-04 19:06:04 -0500

    populate method defined

--- Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
+++ Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
@@ -13,11 +13,28 @@ namespace Lab01_Exception_Handling
             Console.ReadLine();
         }
 
+        // Start sequence method that will be close to the bottom of the call stack where all my other methods will run in succession
         static void StartSequence()
         {
             Console.Write("Please enter a number greater than 0: ");
             string userDefinedArrayLength = Console.ReadLine();
-            Console.Write(userDefinedArrayLength);
+            int[] userDefinedArray = new int[Convert.ToInt32(userDefinedArrayLength)];
+
+            Populate(userDefinedArray);
+            Console.WriteLine(string.Join(",", userDefinedArray));
+
+        }
+
+        static int[] Populate(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Please enter number {0} of {1}: ", i + 1, array.Length);
+                string userValue = Console.ReadLine();
+                Console.WriteLine(userValue);
+                array[i] = int.Parse(userValue);
+            }
+            return array;
         }
 
     }
