eca7917dec9fde6e8dab224500fb7b7f1c37a953
cnickels21 <cnickels_21@yahoo.com>
2020-05-04 20:39:28 -0500

    custom exception added to GetSum

--- Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
+++ Lab01-Exception-Handling/Lab01-Exception-Handling/Program.cs
@@ -14,7 +14,7 @@ namespace Lab01_Exception_Handling
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Oh no! Something went wrong: {0}", ex);
+                Console.WriteLine("Oh no! Something went wrong: {0}", ex.Message);
             }
             finally
             {
@@ -27,21 +27,32 @@ namespace Lab01_Exception_Handling
         // Start sequence method that will be close to the bottom of the call stack where all my other methods will run in succession
         static void StartSequence()
         {
-            // Ask the user for a number to define array length
-            Console.Write("Please enter a number greater than 0: ");
-            string userDefinedArrayLength = Console.ReadLine();
-            Console.WriteLine(userDefinedArrayLength);
+            try
+            {
+                // Ask the user for a number to define array length
+                Console.Write("Please enter a number greater than 0: ");
+                string userDefinedArrayLength = Console.ReadLine();
+                Console.WriteLine(userDefinedArrayLength);
 
-            // Instantiate array where user defines the length
-            int[] userDefinedArray = new int[Convert.ToInt32(userDefinedArrayLength)];
+                // Instantiate array where user defines the length
+                int[] userDefinedArray = new int[Convert.ToInt32(userDefinedArrayLength)];
 
-            // Subsequent methods are called here for the full number game
-            Populate(userDefinedArray);
-            string stringifiedArray = string.Join(",", userDefinedArray); // Variable to hold array as string for console display
+                // Subsequent methods are called here for the full number game
+                Populate(userDefinedArray);
+                string stringifiedArray = string.Join(",", userDefinedArray); // Variable to hold array as string for console display
 
-            Console.WriteLine("Your array size is : {0}", userDefinedArray.Length);
-            Console.WriteLine("The numbers in the array are {0} ", stringifiedArray);
-            Console.WriteLine("Your sum is {0}", GetSum(userDefinedArray));
+                Console.WriteLine("Your array size is : {0}", userDefinedArray.Length);
+                Console.WriteLine("The numbers in the array are {0} ", stringifiedArray);
+                Console.WriteLine("Your sum is {0}", GetSum(userDefinedArray));
+            }
+            catch (FormatException fex)
+            {
+                Console.WriteLine("FOOORRRMAT: {0}", fex.Message);
+            }
+            catch (OverflowException oex)
+            {
+                Console.WriteLine("That number is out of bounds {0}", oex.Message);
+            }
 
         }
 
@@ -58,16 +69,36 @@ namespace Lab01_Exception_Handling
             return array;
         }
 
-        // Method to get the sum of
+        // Method to get the sum of a given array
         static int GetSum(int[] array)
         {
             int sum = 0;
-            for (int i = 0; i < array.Length; i++)
+
+            try
             {
-                sum += array[i];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                ValidateSum(sum);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Value of {0} is too low: {1}", sum, ex);
+            }
+
             return sum;
         }
+
+        // Custom exception handler for GetSum method
+        static void ValidateSum(int sum)
+        {
+            if (sum < 20)
+                throw new Exception(sum.ToString());
+            
+        }
+
+
 
     }
 }
